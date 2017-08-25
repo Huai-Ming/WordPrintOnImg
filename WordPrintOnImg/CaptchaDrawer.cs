2bbf706a9148f845be7f9f5cbf13a2e2f1b91cee
Hyman <1604723751@qq.com>
2017-08-25 18:00:08 +0800

    add captcha operations

---
+++ WordPrintOnImg/CaptchaDrawer.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPrintOnImg
+{
+    public class CaptchaDrawer
+    {
+        //验证码的左上角(起点)的x y
+        private int mCaptchaWidth = 50;
+        private int mCaptchaHeight = 50;
+
+        //验证码滑块的宽高
+        private int mCaptchaX;
+        private int mCaptchaY;
+
+        //控件的宽高
+        protected int mWidth;
+        protected int mHeight;
+
+        //验证码 阴影、抠图的Path
+        private GraphicsPath mCaptchaPath;
+
+        private Random random = new Random();
+
+        public CaptchaDrawer(int width, int height)
+        {
+            this.mWidth = width;
+            this.mHeight = height;
+        }
+
+        public GraphicsPath GetCaptchaPath()
+        {
+            mCaptchaPath = new GraphicsPath();
+            CreateCaptchaPath();
+            return mCaptchaPath;
+        }
+
+        public int CaptchaX
+        {
+            get { return mCaptchaX; }
+        }
+
+        public int CaptchaY
+        {
+            get { return mCaptchaY; }
+        }
+
+        public void CreateCaptchaPath()
+        {
+            //原本打算随机生成gap，后来发现 宽度/3 效果比较好，
+            int gap = random.Next(mCaptchaWidth / 2);
+            gap = mCaptchaWidth / 3;
+
+            //随机生成验证码阴影左上角 x y 点，
+            mCaptchaX = random.Next(mWidth - mCaptchaWidth - gap);
+            mCaptchaY = random.Next(mHeight - mCaptchaHeight - gap);
+
+            mCaptchaPath.Reset();
+
+
+            //从左上角开始 绘制一个不规则的阴影
+            mCaptchaPath.AddLine(mCaptchaX, mCaptchaY, mCaptchaX + gap, mCaptchaY);
+
+            //左上角
+            DrawPartCircle(new PointF(mCaptchaX + gap, mCaptchaY), new PointF(mCaptchaX + gap*2, mCaptchaY),
+                mCaptchaPath, random.Next(2) > 0);
+            mCaptchaPath.AddLine(mCaptchaX + gap * 2, mCaptchaY, mCaptchaX + mCaptchaWidth, mCaptchaY);
+
+            mCaptchaPath.AddLine(mCaptchaX + mCaptchaWidth, mCaptchaY, mCaptchaX + mCaptchaWidth, mCaptchaY + gap);
+            //右上角
+            DrawPartCircle(new PointF(mCaptchaX + mCaptchaWidth, mCaptchaY + gap),
+                new PointF(mCaptchaX + mCaptchaWidth, mCaptchaY + gap * 2),
+                mCaptchaPath, random.Next(2) > 0);
+            mCaptchaPath.AddLine(mCaptchaX + mCaptchaWidth, mCaptchaY + 2 * gap, mCaptchaX + mCaptchaWidth, mCaptchaY + mCaptchaHeight);
+
+            mCaptchaPath.AddLine(mCaptchaX + mCaptchaWidth, mCaptchaY + mCaptchaHeight, mCaptchaX + 2* gap, mCaptchaY + mCaptchaHeight);
+            //右下角
+            DrawPartCircle(new PointF(mCaptchaX + mCaptchaWidth - gap, mCaptchaY + mCaptchaHeight),
+                new PointF(mCaptchaX + mCaptchaWidth - gap * 2, mCaptchaY + mCaptchaHeight),
+                mCaptchaPath, random.Next(2) > 0);
+            mCaptchaPath.AddLine(mCaptchaX + gap, mCaptchaY + mCaptchaHeight, mCaptchaX, mCaptchaY + mCaptchaHeight);
+            mCaptchaPath.AddLine(mCaptchaX, mCaptchaY + mCaptchaHeight, mCaptchaX, mCaptchaY + 2* gap);
+            //左下角
+            DrawPartCircle(new PointF(mCaptchaX, mCaptchaY + mCaptchaHeight - gap),
+                new PointF(mCaptchaX, mCaptchaY + mCaptchaHeight - gap * 2),
+                mCaptchaPath, random.Next(2) > 0);
+            mCaptchaPath.AddLine(mCaptchaX, mCaptchaY + gap, mCaptchaX, mCaptchaY);
+
+        }
+
+        public void DrawPartCircle(PointF start, PointF end, GraphicsPath path, bool outer)
+        {
+            float c = 0.551915024494f;
+            PointF middle = new PointF(start.X + (end.X - start.X) / 2, start.Y + (end.Y - start.Y) / 2);
+            float r1 = (float)Math.Sqrt(Math.Pow((middle.X - start.X), 2) + Math.Pow((middle.Y - start.Y), 2));
+            float gap1 = r1 * c;
+
+            if (Math.Abs(start.X - end.X) < 0.1)
+            {
+                bool topToBottom = end.Y - start.Y > 0 ? true : false;
+
+                int flag; //旋转系数
+
+                if (topToBottom)
+                {
+                    flag = 1;
+                }
+                else
+                {
+                    flag = -1;
+                }
+                if (outer)
+                {
+                    PointF[] array = new PointF[7];
+                    var point1 = new PointF((int)(start.X + gap1 * flag), (int)start.Y);
+                    var point2 = new PointF((int)(middle.X + r1 * flag), (int)(middle.Y - gap1 * flag));
+                    var point3 = new PointF((int)(middle.X + r1 * flag), (int)middle.Y);
+                    var point4 = new PointF((int)(middle.X + r1 * flag), (int)(middle.Y + gap1 * flag));
+                    var point5 = new PointF((int)(end.X + gap1 * flag), (int)end.Y);
+                    var point6 = new PointF((int)(end.X), (int)end.Y);
+                    array[0] = point1;
+                    array[1] = point2;
+                    array[2] = point3;
+                    array[3] = point4;
+                    array[4] = point5;
+                    array[5] = point6;
+                    array[6] = point6;
+                    path.AddBeziers(array);
+                    //凸的 两个半圆
+                    //path
+
+                    //path.(start.X + gap1 * flag, start.y,
+                    //        middle.X + r1 * flag, middle.y - gap1 * flag,
+                    //        middle.x + r1 * flag, middle.y);
+                    //path.cubicTo(middle.x + r1 * flag, middle.y + gap1 * flag,
+                    //        end.x + gap1 * flag, end.y,
+                    //        end.x, end.y);
+                }
+                else
+                {
+                    PointF[] array = new PointF[7];
+                    var point1 = new PointF((int)(start.X - gap1 * flag), (int)start.Y);
+                    var point2 = new PointF((int)(middle.X - r1 * flag), (int)(middle.Y - gap1 * flag));
+                    var point3 = new PointF((int)(middle.X - r1 * flag), (int)middle.Y);
+                    var point4 = new PointF((int)(middle.X - r1 * flag), (int)(middle.Y + gap1 * flag));
+                    var point5 = new PointF((int)(end.X - gap1 * flag), (int)end.Y);
+                    var point6 = new PointF((int)(end.X), (int)end.Y);
+                    array[0] = point1;
+                    array[1] = point2;
+                    array[2] = point3;
+                    array[3] = point4;
+                    array[4] = point5;
+                    array[5] = point6;
+                    array[6] = point6;
+                    path.AddBeziers(array);
+                    ////凹的 两个半圆
+                    //path.cubicTo(start.x - gap1 * flag, start.y,
+                    //        middle.x - r1 * flag, middle.y - gap1 * flag,
+                    //        middle.x - r1 * flag, middle.y);
+                    //path.cubicTo(middle.x - r1 * flag, middle.y + gap1 * flag,
+                    //        end.x - gap1 * flag, end.y,
+                    //        end.x, end.y);
+                }
+            }
+            else
+            {
+                bool leftToRight = end.X - start.X > 0 ? true : false;
+                //以下是我写出了所有的计算公式后推的，不要问我过程，只可意会。
+                int flag;//旋转系数
+                if (leftToRight)
+                {
+                    flag = 1;
+                }
+                else
+                {
+                    flag = -1;
+                }
+                if (outer)
+                {
+                    PointF[] array = new PointF[7];
+                    var point1 = new PointF((int)(start.X), (int)(start.Y - gap1 * flag));
+                    var point2 = new PointF((int)(middle.X - gap1 * flag), (int)(middle.Y - r1 * flag));
+                    var point3 = new PointF((int)(middle.X), (int)(middle.Y - r1 * flag));
+                    var point4 = new PointF((int)(middle.X + gap1 * flag), (int)(middle.Y - r1 * flag));
+                    var point5 = new PointF((int)(end.X), (int)(end.Y - gap1 * flag));
+                    var point6 = new PointF((int)(end.X), (int)end.Y);
+                    array[0] = point1;
+                    array[1] = point2;
+                    array[2] = point3;
+                    array[3] = point4;
+                    array[4] = point5;
+                    array[5] = point6;
+                    array[6] = point6;
+                    path.AddBeziers(array);
+
+                    ////凸 两个半圆
+                    //path.cubicTo(start.x, start.y - gap1 * flag,
+                    //        middle.x - gap1 * flag, middle.y - r1 * flag,
+                    //        middle.x, middle.y - r1 * flag);
+                    //path.cubicTo(middle.x + gap1 * flag, middle.y - r1 * flag,
+                    //        end.x, end.y - gap1 * flag,
+                    //        end.x, end.y);
+                }
+                else
+                {
+                    PointF[] array = new PointF[7];
+                    var point1 = new PointF((int)(start.X), (int)(start.Y + gap1 * flag));
+                    var point2 = new PointF((int)(middle.X - gap1 * flag), (int)(middle.Y + r1 * flag));
+                    var point3 = new PointF((int)(middle.X), (int)(middle.Y + r1 * flag));
+                    var point4 = new PointF((int)(middle.X + gap1 * flag), (int)(middle.Y + r1 * flag));
+                    var point5 = new PointF((int)(end.X), (int)(end.Y + gap1 * flag));
+                    var point6 = new PointF((int)(end.X), (int)end.Y);
+                    array[0] = point1;
+                    array[1] = point2;
+                    array[2] = point3;
+                    array[3] = point4;
+                    array[4] = point5;
+                    array[5] = point6;
+                    array[6] = point6;
+                    path.AddBeziers(array);
+                    ////凹 两个半圆
+                    //path.cubicTo(start.x, start.y + gap1 * flag,
+                    //        middle.x - gap1 * flag, middle.y + r1 * flag,
+                    //        middle.x, middle.y + r1 * flag);
+                    //path.cubicTo(middle.x + gap1 * flag, middle.y + r1 * flag,
+                    //        end.x, end.y + gap1 * flag,
+                    //        end.x, end.y);
+                }
+            }
+        }
+    }
+}
---
+++ WordPrintOnImg/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPrintOnImg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteTextOnImg();
+            //CutPic();
+
+            //Point[] myArray =
+            // {
+            //     new Point(363, 259),
+            //     new Point(365, 257),
+            //     new Point(368, 257),
+            //     new Point(370, 257),
+            //     new Point(373, 259),
+            //     new Point(373, 262),
+            //     new Point(373, 262),
+            // };
+
+            //// Create the path and add the curves.
+            //GraphicsPath myPath = new GraphicsPath();
+            //myPath.AddBeziers(myArray);
+
+            //// Draw the path to the screen.
+            //Pen myPen = new Pen(Color.Red, 2);
+            //Bitmap bmp = new Bitmap(800, 800); // Determining Width and Height of Source Image
+            //Graphics graphics = Graphics.FromImage(bmp);
+            //graphics.DrawPath(myPen, myPath);
+            //bmp.Save("test.jpg", ImageFormat.Jpeg);
+            //graphics.Dispose();
+            //bmp.Dispose();
+
+            //Bitmap bmp = new Bitmap(500, 500); // Determining Width and Height of Source Image
+            //Graphics g = Graphics.FromImage(bmp);
+            //// Create a graphics path
+            //CaptchaDrawer drawer = new CaptchaDrawer(500, 500);
+
+            //// Draw path
+            //Pen redPen = new Pen(Color.Red, 2);
+            //g.DrawPath(redPen, drawer.GetCaptchaPath());
+            //bmp.Save("test.jpg", ImageFormat.Jpeg);
+            //// Dispose of objects
+            //redPen.Dispose();
+            //g.Dispose();
+
+            SetClipPathCombine();
+            Console.Read();
+        }
+
+        public static void SetClipPathCombine()
+        {
+            
+            string oriPicPath = "iceberg_cut.jpg";
+            System.Drawing.Image bitmap = (System.Drawing.Image)Bitmap.FromFile(oriPicPath); // set image     
+            //draw the image object using a Graphics object    
+            Bitmap bmp = new Bitmap(bitmap.Width, bitmap.Height);
+            Graphics graphicsImage = Graphics.FromImage(bmp);
+            CaptchaDrawer drawer = new CaptchaDrawer(bitmap.Width, bitmap.Height);
+            var path = drawer.GetCaptchaPath();
+            path.CloseAllFigures();
+            //graphicsImage.SetClip(path);
+            graphicsImage.SetClip(path);
+            //graphicsImage.FillPath(new SolidBrush(Color.Red), path);
+            graphicsImage.DrawImage(bitmap, new Point(0,0));
+
+           
+            bmp.Save("test1.jpg", ImageFormat.Jpeg);
+            System.Drawing.Image test1 = (System.Drawing.Image)Bitmap.FromFile("test1.jpg");
+            var temp = new Bitmap(test1, new Size(50, 50));
+            temp.Save("test2.jpg", ImageFormat.Jpeg);
+            graphicsImage.Dispose();
+            bitmap.Dispose();
+        }
+
+        public static void CutPic()
+        {
+            string oriPicPath ="iceberg_cut.jpg";//原始图片全路径
+            string m_CutPicPath = System.IO.Path.GetExtension(oriPicPath);
+            m_CutPicPath = oriPicPath.Substring(0, oriPicPath.Length - m_CutPicPath.Length) + "_cut" + m_CutPicPath;//裁剪图片全路径
+
+            int bIsSave = cutPicAndSave(oriPicPath, m_CutPicPath, 10, 10, 150, 150);
+        }
+
+        public static int cutPicAndSave(string oldPath, string newPath, int x, int y, int width, int height)
+        {
+
+            Rectangle cropArea = new System.Drawing.Rectangle(x, y, width, height); //要截取的区域大小
+            Image img = System.Drawing.Image.FromStream(new System.IO.MemoryStream(System.IO.File.ReadAllBytes(oldPath)));//加载图片
+
+            //判断超出的位置否
+            if ((img.Width < x + width) || img.Height < y + height)
+            {
+                //logger.Warn("截取的区域超过了图片本身的高度、宽度.");
+                img.Dispose();
+                return 0;
+            }
+            Bitmap bmpImage = new System.Drawing.Bitmap(img);
+           
+          
+
+            Bitmap bmpCrop = bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+            bmpCrop.Save(newPath);//保存成新文件
+            ChangeOpacity(img);
+            //释放对象
+            img.Dispose();
+            bmpCrop.Dispose();
+            return 1;
+
+        }
+
+
+      
+
+        public static void ChangeOpacity(Image img)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height); // Determining Width and Height of Source Image
+            Graphics graphics = Graphics.FromImage(bmp);
+            
+            ColorMatrix colormatrix = new ColorMatrix();
+            colormatrix.Matrix33 = 10;
+            ImageAttributes imgAttribute = new ImageAttributes();
+            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            graphics.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
+            graphics.Dispose();   // Releasing all resource used by graphics 
+            bmp.Save("iceberg_transpareny.jpg", ImageFormat.Jpeg);
+            bmp.Dispose();
+        }
+
+
+        private static void WriteTextOnImg()
+        {
+//creating a image object    
+            System.Drawing.Image bitmap = (System.Drawing.Image) Bitmap.FromFile("iceberg.jpg"); // set image     
+            //draw the image object using a Graphics object    
+            Graphics graphicsImage = Graphics.FromImage(bitmap);
+
+            //Set the alignment based on the coordinates       
+            StringFormat stringformat = new StringFormat();
+            stringformat.Alignment = StringAlignment.Far;
+            stringformat.LineAlignment = StringAlignment.Far;
+
+            StringFormat stringformat2 = new StringFormat();
+            stringformat2.Alignment = StringAlignment.Center;
+            stringformat2.LineAlignment = StringAlignment.Center;
+
+            //Set the font color/format/size etc..      
+            Color StringColor = System.Drawing.ColorTranslator.FromHtml("#933eea"); //direct color adding    
+            Color StringColor2 = System.Drawing.ColorTranslator.FromHtml("#e80c88"); //customise color adding    
+            string Str_TextOnImage = "学"; //Your Text On Image    
+            string Str_TextOnImage2 = "宇"; //Your Text On Image    
+
+            graphicsImage.DrawString(Str_TextOnImage, new Font("arial", 40,
+                FontStyle.Regular), new SolidBrush(StringColor), new Point(130, 245),
+                stringformat);
+
+
+            graphicsImage.DrawString(Str_TextOnImage2, new Font("Edwardian Script ITC", 111,
+                FontStyle.Bold), new SolidBrush(StringColor2), new Point(345, 255),
+                stringformat2);
+
+           
+            bitmap.Save("iceberg1.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
